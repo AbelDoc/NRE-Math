9ecfdc79c04f3d97e218237dbc6e18bc8821f0e3
Louis Abel <louis.abel5@etu.univ-lorraine.fr>
2019-10-20 17:49:24 +0200

    Change main demo

--- src/NRE_Main.cpp
+++ src/NRE_Main.cpp
@@ -12,17 +12,45 @@
     using namespace NRE::Math;
 
     int main(int, char**) {
-        float f = 0;
-        for (int i = 0; i < 100; i++) {
-            f += 0.01f;
-        }
-
-        int i = 1;
-        std::cout << equal(i, f) << std::endl;
-        
-        std::cout << std::is_pod_v<Matrix3x3<float>> << std::endl;
-        std::cout << std::is_pod_v<Matrix4x4<float>> << std::endl;
-        std::cout << std::is_pod_v<Vector3D<float>[3]> << std::endl;
+        Vector4D<float> u({0, 1, 0, 0});
+        Vector4D<float> v({1, 0, 0, 0});
+    
+        std::cout << u << std::endl;
+        std::cout << v << std::endl;
+    
+        Vector4D<float> w(u ^ v);       // Cross product
+    
+        std::cout << w << std::endl;
+        std::cout << (u | v) << std::endl;  // Dot product
+    
+        Segment4D<float> seg(Vector3D<float>(0, 0, 0), Vector3D<float>(0, 100, 0));
+    
+        std::cout << seg << std::endl;
+    
+        Matrix4x4<float> mat;
+        mat.setIdentity();
+        mat.rotate(90_deg, Vector3D<float>(1, 0, 0));  // Equivalent of mat.rotate(1_rad, {1, 0, 0});
+    
+        u = mat * u;
+        v = mat * v;
+        w = mat * w;
+    
+        std::cout << u << std::endl;
+        std::cout << v << std::endl;
+        std::cout << w << std::endl;
+    
+        seg = mat * seg;
+    
+        std::cout << seg << std::endl;
+    
+        Quaternion<float> quat(Vector3D<float>(1, 0, 0), 90_deg);      // Equivalent to previous matrix,
+                                                        // if 4th parameter is not an Angle it only set parameters
+    
+        std::cout << quat << std::endl;
+    
+        Matrix4x4<float> quatToMat(quat);
+    
+        std::cout << quatToMat << std::endl;
 
         return 0;
     }
